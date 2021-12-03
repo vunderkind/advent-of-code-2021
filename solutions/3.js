724fbd9a64b630d6a5251fb156b724373cdb09c3
Justin Irabor <justin.irabor@gmail.com>
2021-12-03 16:44:55 +0100

    Completed day 3

--- solutions/3.js
+++ solutions/3.js
@@ -12,15 +12,20 @@ readFile('../files/december-3.txt', 'utf8', (err, data) => {
     console.log('\n')
 
     console.log('--02 and CO2 reading below--')
-    calculateO2andCO2Rating(data);
+
+    let rating = calculateO2andCO2Rating(dataArray);
+
+    console.log('Rating: ', rating);
+    console.log('--------------')
 
 })
 
+// The gamble for silver
 function calculateGammaAndEpsilon(data) {
-    let gamma;
-    let epsilon;
     let ones = Array(12).fill(0);
     let zeroes = Array(12).fill(0);
+    let gamma;
+    let epsilon;
 
     for (let i = 0; i < data.length; i++) {
         for (let j = 0; j < data[i].length; j++) {
@@ -50,8 +55,64 @@ function calculateGammaAndEpsilon(data) {
     console.log('Submarine gamma rate:', parseInt(gamma, 2));
     console.log('Submarine epsilon rate:', parseInt(epsilon, 2));
     console.log('Submarine power consumption:', parseInt(gamma, 2) * parseInt(epsilon, 2));
+    gammaData = gamma;
+    epsilonData = epsilon;
 }
 
+
+// Going for gold
+// Write small function that loops through the main 
 function calculateO2andCO2Rating(data) {
-    console.log('WIP!')
+    let o2val = tracking(true, data);
+    let co2val = tracking(false, data);
+    
+    let a = parseInt(o2val.toString(), 2);
+    let b = parseInt(co2val.toString(), 2);
+
+    console.log('O2 reading: ', a);
+    console.log('CO2 reading: ', b);
+    
+    return a * b;
+}
+
+function tracking(oxygen, data) {
+    let onecounter = 0;
+    let zerocounter = 0;
+    let tempTracker = []
+    let thingToTrack = data
+    
+    for (let i = 0; i < 12; i++) {
+        for (let j = 0; j < thingToTrack.length; j++) {
+            if (thingToTrack[j][i] === '1') {
+                onecounter++
+            } else if (thingToTrack[j][i] === '0') {
+                zerocounter++
+            }
+        }
+        for (let k = 0; k < thingToTrack.length; k++) {
+                if (thingToTrack.length == 1) {
+                        tempTracker = thingToTrack;
+                        return thingToTrack;
+                    }
+                if (oxygen) {
+                    if (onecounter >= zerocounter && thingToTrack[k][i] === '1') {
+                        tempTracker.push(thingToTrack[k]);
+                    } else if (onecounter < zerocounter && thingToTrack[k][i] === '0') {
+                        tempTracker.push(thingToTrack[k]);
+                    }
+                } else {
+                    if (onecounter < zerocounter && thingToTrack[k][i] === '1') {
+                        tempTracker.push(thingToTrack[k]);
+                    } else if (zerocounter <= onecounter && thingToTrack[k][i] === '0') {
+                        tempTracker.push(thingToTrack[k]);
+                    }
+                }
+            }
+
+        thingToTrack = tempTracker;
+        tempTracker = []
+        onecounter = 0;
+        zerocounter = 0;
+    };
+    return thingToTrack;
 }
