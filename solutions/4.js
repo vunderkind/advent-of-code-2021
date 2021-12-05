acc6b0e4519665879541c382f7cd04871b0a6e27
Justin Irabor <justin.irabor@gmail.com>
2021-12-05 16:44:39 +0100

    day four done

--- solutions/4.js
+++ solutions/4.js
@@ -13,66 +13,65 @@ readFile('../files/december-4-1.txt', 'utf-8', (err, looper) => {
 })
 
 const playBingo = (callout, bingo) => {
-    let isInProgress = true;
-    let results;
-    let boardWithCompleteRows = false;
-    let items = []
 
-        
-        // Continue to go through the list of bingo boards
-        for (let i = 0; i < callout.length; i++) {
-            if (boardWithCompleteRows === true) {
-                items.push(results);
-                console.log(items);
-                break;
-            }
-            for (let j = 0; j < bingo.length; j++) {
-                for (let k = 0; k < bingo[j].length; k++) {
+    // keep a cache of board positions that have won
+    let cache = []
 
-                    // Check the bingo board for a match
+    // keep a cache of winning numbers
+    const winningNumber = []
+
+    // time for some fugly loops!
+    for (let i = 0; i < callout.length; i++) {
+        for (let j = 0; j < bingo.length; j++) {
+            for (let k = 0; k < bingo[j].length; k++) {
+
+                // Check the bingo board for a match
+                if (!cache.includes(j)) {
                     if (bingo[j][k] === callout[i]) {
                         bingo[j][k] = '🐙';
+                        checkBingo(bingo[j], cache, j, winningNumber, callout[i])
                     }
-                    
-                    // Check if the boards have a full cross
-                    let status = checkBingo(bingo[j]);
-                    if (status) {
-                        boardWithCompleteRows = status;
-                        
-                        results = {
-                            ['Board number']: j,
-                            ['The board']: bingo[j],
-                            ['What\'s left']: sumUnmarkedSlots(bingo[j]),
-                            ['Winning callout']: callout[i],
-                            ['Multiplying what\'s left with the winning callout']: sumUnmarkedSlots(bingo[j]) * callout[i]
-                        }
-                        isInProgress = false;
-                        break;
-                    }
-                    
                 }
             }
 
+        }
+    }
+    const results = {
+        winner: {
+            ["Winning board number"]: cache[0],
+            ["Winning board"]: bingo[cache[0]],
+            ["Sum of unmarked slots"]: sumUnmarkedSlots(bingo[cache[0]]),
+            ["Winning numbers"]: winningNumber[0],
+            ["Puzzle winning score"]: winningNumber[0] * sumUnmarkedSlots(bingo[cache[0]])
+        },
+        loser: {
+            ["Winning board number"]: cache[cache.length-1],
+            ["Winning board"]: bingo[cache[cache.length-1]],
+            ["Sum of unmarked slots"]: sumUnmarkedSlots(bingo[cache[cache.length-1]]),
+            ["Winning numbers"]: winningNumber[winningNumber.length-1],
+            ["Puzzle winning score"]: winningNumber[winningNumber.length-1] * sumUnmarkedSlots(bingo[cache[cache.length-1]])
+        }
     }
 
-    // console.log('Predicting the winning bingo: ', results);
+    console.log(results);
 
 }
 
-function checkBingo(array) {
+function checkBingo(array, cache, position, winningNumber, number) {
     // Count five times, and at each stop, check the row and columns
-    
     for (let i = 0; i <= 5; i++) {
         for (let j = 0; j < 24; j += 5) {
             if (
                 array[i] === '🐙' && array[i + 5] === '🐙' && array[i + 10] === '🐙' && array[i + 15] === '🐙' && array[i + 20] === '🐙'
                 || array[j] === '🐙' && array[j + 1] === '🐙' && array[j + 2] === '🐙' && array[j + 3] === '🐙' && array[j + 4] === '🐙'
             ) {
-                return true;
+                cache.push(position);
+                winningNumber.push(number);
+                    return true;
             }
         }
     }
-    return false;
+    return;
 }
 
 //Sort the data from the puzzle input into a a standard array (for the bingo numbers)
